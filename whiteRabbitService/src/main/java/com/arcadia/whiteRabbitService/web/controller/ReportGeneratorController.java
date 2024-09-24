63af905c11b967e9577ade812dac3f5efd397f7c
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-09-24 10:30:03 +0800

    Move file generation logic to service layer

--- whiteRabbitService/src/main/java/com/arcadia/whiteRabbitService/web/controller/ReportGeneratorController.java
+++ whiteRabbitService/src/main/java/com/arcadia/whiteRabbitService/web/controller/ReportGeneratorController.java
@@ -1,12 +1,7 @@
 package com.arcadia.whiteRabbitService.web.controller;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-import org.ohdsi.rabbitInAHat.dataModel.ETL;
-import org.ohdsi.rabbitInAHat.dataModel.ETL.FileFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.arcadia.whiteRabbitService.service.ReportGenerationService;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,22 +23,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ReportGeneratorController {
   private final ReportGenerationService reportGenerationService;
-  private final ObjectMapper objectMapper;
 
   @PostMapping("/word")
     public ResponseEntity<byte[]> generateWordReport_test(@RequestBody JsonNode jsonNode) {
         try {
-            // Create a temporary file and write the JSON content to it
-            Path tempFile = Files.createTempFile("etl-temp", ".json");
-            File file = tempFile.toFile();
-            
-            // Write the JSON data to the temporary file
-            objectMapper.writeValue(file, jsonNode);
-
-            ETL etl = ETL.fromFile(file.getAbsolutePath(), FileFormat.Json);
-
-            ByteArrayOutputStream wordDocument = reportGenerationService.generateWordReport(etl);
-
+            ByteArrayOutputStream wordDocument = reportGenerationService.generateWordReport(jsonNode);
             // Return the file as a response
             HttpHeaders headers = new HttpHeaders();
             headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etl-report.docx");
